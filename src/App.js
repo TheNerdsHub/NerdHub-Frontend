e405a7f7f35c57485155105bb91a9479d65632ec
Alexander McCutcheon <snowpuppy4290@gmail.com>
2024-01-09 01:33:08 -0600

    Setting Up Modular Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import GamesPage from './components/GamesPage';
+import QuotePage from './components/QuotesPage';
+import TimelinePage from './components/TimelinePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/games" component={GamesPage} />
+        <Route path="/quotes" component={QuotePage} />
+        <Route path="/timeline" component={TimelinePage} />
+      </Switch>
+    </Router>
   );
 }
 
