52ec53b7d774ada142e503a7a2fa0c46692e1968
Alexander <snowpuppy4290@gmail.com>
2026-06-27 16:43:11 -0500

    Rename API_URL to API_URL_EXTERNAL in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 cat > /usr/share/nginx/html/config.js << EOF
-window.__API_ROOT__ = "${API_URL:-http://localhost:5172}";
+window.__API_ROOT__ = "${API_URL_EXTERNAL:-http://localhost:5172}";
 EOF
 
 exec nginx -g "daemon off;"
